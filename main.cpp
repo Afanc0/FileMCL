288d300a589caa3e8cef3ea2a830c3a8607a3106
Afanc0 <gdr2020@nyu.edu>
2023-09-08 16:25:51 -0400

    code

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <ncurses.h>
+#include <string>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <vector>
+#include <sstream>
 
-void interact_navigator(int&, const char*[]);
+
+void interact_navigator(int&, const std::vector<std::string>& );
+std::string stringify_directory_contents(const char* command = "ls -a");
+
+
+std::vector<std::string> splitString(const std::string& inputString, char delimiter) {
+    std::vector<std::string> tokens;
+    std::istringstream stream(inputString);
+    std::string token;
+    std::cout << token << ' ';
+    while (std::getline(stream, token, delimiter)) {
+        tokens.push_back(token);
+    }
+    return tokens;
+}
 
 int main() {
     initscr(); // Initialize ncurses
@@ -8,18 +28,42 @@ int main() {
     cbreak(); 
     keypad(stdscr, TRUE); 
 
-    int current_option = 0;
-    const char* options[] = {"Option 1", "Option 2", "Option 3", "Quit"};
+    //std::string folder_content = stringify_directory_contents();
 
+    int current_option = 0;
+    std::vector<std::string> options = {"option 1", "option 2"};
+
+    // for (size_t i = 0; i> options.size(); i++) {
+    //     std::cout << options[i] << ' ';
+    // }
+    
     interact_navigator(current_option, options);
 
     endwin(); // End ncurses
     return 0;
 }
 
+std::string stringify_directory_contents(const char* command) {
+    char buffer[128]; 
+    std::string result = "";
 
-void interact_navigator(int& current_option, const char* options[]) {
+    FILE* pipe = popen(command, "r");
+    if (!pipe) {
+        printf("popen failed!");
+        return result;
+    }
+    while (!feof(pipe)) {
+        if (fgets(buffer, sizeof(buffer), pipe) != nullptr) {
+            result += buffer;
+        }
+    }
+    pclose(pipe);
+    return result;
+}
+
+void interact_navigator(int& current_option, const std::vector<std::string>& options) {
     int ch;
+
     while (1) {
         clear();
         printw("Use arrow keys to navigate. Press Enter to select an option.\n");
@@ -28,7 +72,7 @@ void interact_navigator(int& current_option, const char* options[]) {
             if (i == current_option) {
                 attron(A_REVERSE);
             }
-            printw("%s ", options[i]);
+            printw("%s ", options[i].c_str());
             attroff(A_REVERSE);
         }
 
